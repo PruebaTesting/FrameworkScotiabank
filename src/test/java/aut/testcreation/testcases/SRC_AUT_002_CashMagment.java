a17eaf78fc7e7498f5c0ddd5c808195742e13718
Leonel <leonel_moreno99@hotmail.com>
2023-01-16 17:50:42 -0300

    Nuevos test y refactorización

--- src/test/java/aut/testcreation/testcases/SRC_AUT_002_CashMagment.java
+++ src/test/java/aut/testcreation/testcases/SRC_AUT_002_CashMagment.java
@@ -1,6 +1,8 @@
 package aut.testcreation.testcases;
 
 import framework.engine.selenium.SeleniumTestBase;
+import org.junit.After;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -20,69 +22,32 @@ public class SRC_AUT_002_CashMagment extends SeleniumTestBase {
         Login.correcto("pUsuario", "pContrasenia", "pToken");
     }*/
 
-    @Test
-    void TC_002_01_altaBeneficiarioPagoDeFacturaConNumCliente() {
-        getTestName(getMethodName());
+    @ParameterizedTest
+    @CsvSource({"TC_002_01_altaBeneficiarioPagoDeFacturaConNumCliente",
+                "TC_002_02_altaBeneficiarioPagoDeFacturaSinNumCliente"})
+    void TC_002_01y02_test(String methodName) {
+        getTestName(methodName);
         Login.correcto("pUsuario", "pContrasenia", "pToken");
         CrearBeneficiario.pagoDeServiciosYFacturas("pAlias", "pNumCliente");
     }
 
-    @Test
-    void TC_002_02_altaBeneficiarioPagoDeFacturaSinNumCliente() {
-        getTestName(getMethodName());
-        Login.correcto("pUsuario", "pContrasenia", "pToken");
-        CrearBeneficiario.pagoDeServiciosYFacturas("pAlias");
-    }
-
-    @Test
-    void TC_002_03_altaBeneficiarioPersonaMoralOtroBanco() {
-        getTestName(getMethodName());
+    @ParameterizedTest
+    @CsvSource({"TC_002_03_altaBeneficiarioPersonaMoralOtroBanco",
+                "TC_002_04_altaBeneficiarioPersonaMoralScotiabank"})
+    void TC_002_03y04_test(String methodName) {
+        getTestName(methodName);
         Login.correcto("pUsuario", "pContrasenia", "pToken");
         CrearBeneficiario.personaMoral("pNombrePerfil", "pTipoCuenta", "pNumCuenta", "pNumIdentificacion", "pLimiteAbono");
     }
 
-    @Test
-    void TC_002_04_altaBeneficiarioPersonaMoralScotiabank() {
-        getTestName(getMethodName());
-        Login.correcto("pUsuario", "pContrasenia", "pToken");
-        CrearBeneficiario.personaMoral("pNombrePerfil", "pTipoCuenta", "pNumCuenta", "pLimiteAbono");
-    }
-
-    @Test
-    void TC_002_05_altaBeneficiarioPersonaFisicaOtroBanco() {
-        getTestName(getMethodName());
+    @ParameterizedTest
+    @CsvSource({"TC_002_05_altaBeneficiarioPersonaFisicaOtroBanco",
+                "TC_002_06_altaBeneficiarioPersonaFisicaScotiabank"})
+    void TC_002_05_altaBeneficiarioPersonaFisicaOtroBanco(String methodName) {
+        getTestName(methodName);
         Login.correcto("pUsuario", "pContrasenia", "pToken");
         CrearBeneficiario.personaFisica("pNombrePerfil", "pTipoCuenta", "pNumCuenta", "pNumIdentificacion", "pNombre", "pAoellidoPaterno", "pApellidoMaterno", "pLimiteAbono");
     }
-
-    @Test
-    void TC_002_06_altaBeneficiarioPersonaFisicaScotiabank(){
-        getTestName(getMethodName());
-        Login.correcto("pUsuario", "pContrasenia", "pToken");
-        CrearBeneficiario.personaFisica("pNombrePerfil", "pTipoCuenta", "pNumCuenta", "pLimiteAbono");
-    }
-
-    /*@Test
-    void TC_002_07_altaBeneficiarioContribucionesGubernamentalesEstado(){
-        getTestName(getMethodName());
-        Login.correcto("pUsuario", "pContrasenia", "pToken");
-        CrearBeneficiario.contribucionesGubernamentales("pTipoImpuesto");
-    }*/
-
-    /*@Test
-    void TC_002_07a10_test() throws IOException {
-        String[] arrMethodName = {
-                "TC_002_07_altaBeneficiarioContribucionesGubernamentalesEstado",
-                "TC_002_08_altaBeneficiarioContribucionesGubernamentalesCiudad",
-                "TC_002_09_altaBeneficiarioContribucionesFederalesSAT",
-                "TC_002_10_altaBeneficiarioContribucionesFederalesDerechos"
-        };
-        for (int i = 0; i < 3; i++) {
-            getTestName(arrMethodName[i]);
-            Login.correcto("pUsuario", "pContrasenia", "pToken");
-            CrearBeneficiario.contribucionesGubernamentalesYFederales("pTipoImpuesto");
-        }
-    }*/
 
     @ParameterizedTest
     @CsvSource({"TC_002_07_altaBeneficiarioContribucionesGubernamentalesEstado",
@@ -97,50 +62,65 @@ public class SRC_AUT_002_CashMagment extends SeleniumTestBase {
     }
 
     @Test
-    void TC_002_08_altaBeneficiarioContribucionesGubernamentalesCiudad() {
-        getTestName(getMethodName());
-        Login.correcto("pUsuario", "pContrasenia", "pToken");
-        CrearBeneficiario.contribucionesGubernamentales("pTipoImpuesto");
-    }
-
-    @Test
-    void TC_002_09_altaBeneficiarioContribucionesFederalesSAT() {
-        getTestName(getMethodName());
-        Login.correcto("pUsuario", "pContrasenia", "pToken");
-        CrearBeneficiario.contribucionesFederales("pTipoImpuesto");
-    }
-
-    @Test
-    void TC_002_10_altaBeneficiarioContribucionesFederalesDerechos() {
-        getTestName(getMethodName());
-        Login.correcto("pUsuario", "pContrasenia", "pToken");
-        CrearBeneficiario.contribucionesFederales("pTipoImpuesto");
-    }
-
-    @Test
     void TC_002_11_pagoDeFactura (){
         getTestName(getMethodName());
         Login.correcto("pUsuario", "pContrasenia", "pToken");
-        Realizar.pagoFactura();
+        Pago.factura();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"TC_002_12_pagoDeFacturaParcialConNumCliente",
+                "TC_002_13_pagoDeFacturaTotalConNumCliente"})
+    void TC_002_12y13_test(String pMethodName) {
+        getTestName(pMethodName);
+        Login.correcto("pUsuario", "pContrasenia", "pToken");
+        Pago.facturaConNumCliente("pImporte", "pEsPagoParcial");
     }
 
     @Test
-    void TC_002_12_pagoDeFacturaConNumCliente() {
+    void TC_002_14_pagoCIPARE() {
         getTestName(getMethodName());
         Login.correcto("pUsuario", "pContrasenia", "pToken");
-        Realizar.pagoFacturaConNumCliente("pImporte");
+        Pago.CIPARE("pRegistroPatronal", "pPeriodo");
     }
 
-    @BeforeEach
+    @ParameterizedTest
+    @CsvSource({"TC_002_15_pagoContribucionesGubernamentalesEstado",
+                "TC_002_16_pagoContribucionesGubernamentalesCiudad"})
+    void TC_002_15y16_test(String methodName) {
+        getTestName(methodName);
+        Login.correcto("pUsuario", "pContrasenia", "pToken");
+        Pago.contribucionesGubernamentales("pLineaCaptura", "pImporte", "pFechaLimitePago");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"TC_002_17_pagoContribucionesFederalesDerechos",
+                "TC_002_18_pagoContribucionesFederalesSAT"})
+    void TC_002_17y18_test(String methodName) {
+        getTestName(methodName);
+        Login.correcto("pUsuario", "pContrasenia", "pToken");
+        Pago.contribucionesFederales("pRFC", "pRazonSocial", "pDependenciaYEntidades", "pPeriodicidad", "pEjercicio", "pPeriodo", "pClaveReferenciaDPA", "pCadenaDependencia", "pImporte", "pLineaCaptura");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"TC_002_19_pagoNominaOtroBanco",
+                "TC_002_20_pagoNominaScotiabank"})
+    void TC_002_19y20_test() {
+        getTestName(methodName);
+        Login.correcto("pUsuario", "pContrasenia", "pToken");
+        Pago.pagoNomina("pImporte", "pReferenciaNumerica", "pConcepto");
+    }
+
+    @AfterEach
     @Tag(value = "pagoFacturas")
-    void beforePagoFacturas() {
+    void afterPagoFacturas() {
         Login.confirmarAutenticacion("pContrasenia", "pToken");
         assertTrue(Comprobar.descargaComprobante());
     }
 
-    @BeforeEach
+    @AfterEach
     @Tag(value = "creacionBeneficiario")
-    void beforeCreacionBeneficiario(){
+    void afterCreacionBeneficiario(){
         Login.confirmarAutenticacion("pContrasenia", "pToken");
         assertTrue(Comprobar.creacionBeneficiarioExitosa("pResultadoEsperado"));
     }
